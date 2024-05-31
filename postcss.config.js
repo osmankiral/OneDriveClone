ce0121b869b6c5615ee69d7169f02e9b292dd553
osmankiral <osman.kiral96@gmail.com>
2024-06-01 02:02:20 +0300

    Add tailwindcss to devDependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    // Diğer PostCSS eklentileri buraya eklenebilir
+  ]
 }
